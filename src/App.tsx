1bbf0b2235d9829b8da4fbc3185617fd872cd19d
Mostafa Mojtahedi <mojtahedimostafa.m@gmail.com>
2023-11-22 12:57:08 +0330

    chore: add nextui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <p className="w-auto">Hello world</p>;
+  return (
+    <>
+      <p className="m-3 w-auto p-5 text-center">Hello world</p>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import { NextUIProvider } from '@nextui-org/react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NextUIProvider>
+      <App />
+    </NextUIProvider>
   </React.StrictMode>,
 );
